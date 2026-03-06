53b09ddffe41f74fca0ce1ecf00cc1d17363ed7f
pavithrenaik22 <jaraplapavithra22>
2026-03-07 00:24:31 +0530

    fix db connection for render

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Pavithra@123",
-  database: "schooldb"
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "Pavithra@123",
+  database: process.env.DB_NAME || "schooldb"
 });
 
 db.connect((err) => {
   if (err) {
-    console.log("Database connection failed");
+    console.log("Database connection failed, running without DB");
   } else {
     console.log("MySQL Connected");
   }
